b2752128ae3f74916f827c28a3335204d84ea8a3
Jack Bevis <jack.bevis@gmail.com>
2017-02-01 22:43:26 -0700

    Add ARIA roles to HTML, and cleaned up js comments/code.

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-
+//elements by id
 var userGuess = document.querySelector('#num-guess');
+var displayNum = document.querySelector('#last-guess')
+var feedBack = document.querySelector('#results');
+document.querySelector('#prompt-text')
+
+//button variables
 var guessBtn = document.querySelector('#guess');
 var clearBtn = document.querySelector('#clear');
 var resetBtn = document.querySelector('#reset');
-var displayNum = document.querySelector('#last-guess')
-var feedBack = document.querySelector('#results');
+
+//Other globals
 var maxNum = 100;
 var minNum = 1;
 var randomNumber;
 
-//generate random number
+//generates random number on window load
 function generateNum(min, max) {
   randomNumber = Math.floor(Math.random() * 100 + 1);
   console.log(randomNumber);
 }
-
 window.onload = function () {
   generateNum (minNum, maxNum);
 }
 
 
-
-// guess button and clear button need to be disabled.
-//Set default in HTML to disabled.
-//function to check if input has a value, and reactivates buttons
+//Checks value of input field. If value entered, enables buttons.
 function checkInput () {
   var inputValue = document.getElementById('num-guess').value
   if (inputValue === ""){
@@ -32,18 +33,14 @@ function checkInput () {
     resetBtn.disabled = true;
   };
 };
-
 userGuess.addEventListener('keyup', function(){
   clearBtn.disabled = false;
   guessBtn.disabled = false;
   resetBtn.disabled = false;
 })
 
-//user enters number and submits. Random # is generated, user number is displayed below.
-// within each statement pass queryselector to change #results innertext to appropriate response.
-// if inside of an if....test if number is in range...if it is then run results conditional else alert number is outside the range, try again...
-//if time...new conditional if submittedNum == NaN...error, calls window reload.
 
+//guessBtn event listener. runs game.
 guessBtn.addEventListener('click', function(){
   checkInput();
   var submittedNum = userGuess.value;
@@ -53,13 +50,10 @@ guessBtn.addEventListener('click', function(){
   var feedBack = document.querySelector('#results');
   if (submittedNum == randomNumber) {
     feedBack.innerText = 'BOOM!';
-
   } else if (submittedNum < randomNumber && submittedNum >= minNum) {
     feedBack.innerText =  "That's too low, try again";
-
   } else if (submittedNum > randomNumber  && submittedNum <= maxNum) {
     feedBack.innerText = "That's too high, try again"
-
   } else if (submittedNum < minNum || submittedNum > maxNum) {
     alert("Your guess is outside the range!")
     feedBack.innerText =  "Your guess is outside the range, reset and try again"
@@ -67,13 +61,7 @@ guessBtn.addEventListener('click', function(){
   });
 
 
-
-
-
-
-
-
-// clear button clears user input field
+//clears input field, but won't reset game.
 clearBtn.addEventListener('click', function(){
   userGuess.value = "";
   checkInput();
@@ -81,14 +69,12 @@ clearBtn.addEventListener('click', function(){
 })
 
 
-//reset
-// function to clear out user's submitted number
-// generate new random number once clicked.
+//resets the game and generates new randomNum
 resetBtn.addEventListener('click', function(){
   window.onload();
   userGuess.value = "";
   displayNum.innerText ="?";
-  feedBack.innerText = "Results";
+  feedBack.innerText = "(...Results...)";
   checkInput();
   console.log('Reset Game');
 })
